c8d993b7adc8172612742dad775c183db7fe3b11
RA7 <169072120+Ruslan-A7@users.noreply.github.com>
2025-10-21 21:12:57 +0300

    update

--- src/examples/framework/definition/paths.php
+++ src/examples/framework/definition/paths.php
@@ -33,5 +33,5 @@ define('DIR_INSTALL', DIR_SITE . 'install' . DS);
 /** Директорія з файлами оновлення додатку */
 define('DIR_UPDATE', DIR_SITE . 'update' . DS);
 
-/** Директорія з композер-пакетами додатку */
+/** Директорія з composer-пакетами додатку */
 define('DIR_PACKAGIST_VENDOR', DIR_SITE . 'vendor' . DS);
